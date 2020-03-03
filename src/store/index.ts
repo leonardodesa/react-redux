9fc56610727ee8e7c8ef70dbc210e20048c8e70d
leonardogarupa <leonardo.sa@garupa.co>
2020-03-03 17:25:08 -0300

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, Store } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { RepositoriesState } from './ducks/repositories/types';
+
+import rootReducer from './ducks/rootReducer';
+import rootSaga from './ducks/rootSaga';
+
+export interface ApplicationState {
+  repositories: RepositoriesState
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store: Store<ApplicationState> = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+export default store;
